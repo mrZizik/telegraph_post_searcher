5152d966d1594c144cd8de362d89cec56d162505
mrZizik <mrfreezzzer@mail.ru>
2017-05-11 22:21:39 +0300

    sockets and pool

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def checkurl(query):
 def generate_urls(title, months):
     for i in months:
         for j in range(1,32):
-            yield title + "-" + str(i).zfill(2) + "-" + str(j).zfill(2)
+            yield "-".join([title, str(i).zfill(2), str(j).zfill(2)])
 
 def step(query):
     temp = 1
@@ -22,7 +22,7 @@ def step(query):
     while checkurl(tempquery):
         found.append("http://{}/{} exists".format(URL,tempquery))
         temp += 1
-        tempquery = query + "-" + str(temp)
+        tempquery =  "{}-{}".format(query, str(temp))
     return "\n".join(found)
 
 title = input("Search title: ").lower()
